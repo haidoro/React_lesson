747229b69deee31409a3f19aea592208e8a742b4
Tahara <tahara@studiom-web.net>
2018-05-27 20:47:42 +0900

    propsの使用 modified:   src/components/app.jsx new file:   src/components/greeting.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+
+import Greeting from './greeting';
 
 class App extends Component {
-  render() {
-    return <div>Hello from class App</div>;
-  }
+    render() {
+        return <div>
+            <Greeting name="John"/>
+            <Greeting name="Bob" />
+          </div>;
+    }
 }
 
+
 export default App;
+
