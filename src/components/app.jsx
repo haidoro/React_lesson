4c2975634ecfd7371d89bb30f1e1f438410cb0e8
Tahara <tahara@studiom-web.net>
2018-05-31 20:28:07 +0900

    表示コンポーネント作成 Changes to be committed: new file:   src/components/GeocodeResult.jsx modified:   src/components/app.jsx modified:   stylesheets/index.scss

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult';
 
 class App extends Component {
   constructor(props) {
@@ -11,7 +12,7 @@ class App extends Component {
   }
 
   handlePlaceSubmit(place){
-    console.log(place);
+
   }
 
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
       <div>
         <h1>緯度経度検索</h1>
         <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+        <GeocodeResult address={this.state.address} lat={this.state.lat} lng={this.state.lng} />
       </div>
     );
   }
